828addc1e584edc3ce63178bad794dadc926c176
fsRakib <fsrakiba50@gmail.com>
2024-08-20 02:55:08 +0600

    Minor change- Option switcher

--- src/components/OptionSwitcher.js
+++ src/components/OptionSwitcher.js
@@ -2,13 +2,15 @@ import React from "react";
 
 const OptionSwitcher = ({ options, selectedOption, onSelectOption }) => {
   return (
-    <div className="flex space-x-2 relative bg-slate-400 rounded-lg p-1">
+    <div className="flex space-x-2 relative  rounded-lg ">
       {options.map((option, index) => (
         <button
           key={index}
           onClick={() => onSelectOption(option)}
-          className={`flex-1 px-6 py-1 font-semibold transition-all duration-300 rounded-full ${
-            selectedOption === option ? "bg-black text-white" : "bg-white text-black"
+          className={`flex-1 px-6 py-2 transition-all duration-300 rounded-full ${
+            selectedOption === option
+              ? "bg-black text-white font-bold"
+              : "bg-white text-black font-normal"
           } focus:outline-none`}
         >
           {option}
